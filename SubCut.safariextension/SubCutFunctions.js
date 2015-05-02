d2292168bc53cd74fd77faab725cef47ebfa5a8f
Ryan Batchelder <rb688@nau.edu>
2015-05-02 12:06:26 -0700

    Added shift enter to invert new tab option Reformatting JS to use 4 spaces instead of 2 for indenting

--- SubCut.safariextension/SubCutFunctions.js
+++ SubCut.safariextension/SubCutFunctions.js
@@ -1,25 +1,29 @@
+
 // Open subreddit in a new tab if newTab == true
 function goToSubreddit(newTab) {
-  var sub = document.getElementById("subreddit").value;
-  var subredditStem;
-  if (safari.extension.settings.useHttps) {
-    subredditStem = "https://reddit.com/r/";
-  }
-  else {
-    subredditStem = "http://reddit.com/r/";
-  }
-  if (!newTab) {
-    safari.application.activeBrowserWindow.activeTab.url = subredditStem + sub;
-  }
-  else {
-    var newTab = safari.application.activeBrowserWindow.openTab();
-    newTab.url = subredditStem + sub;
-  }
-  safari.self.hide();
+    var sub = document.getElementById("subreddit").value;
+    var subredditStem;
+    if (safari.extension.settings.useHttps) {
+        subredditStem = "https://reddit.com/r/";
+    } else {
+        subredditStem = "http://reddit.com/r/";
+    }
+    if (!newTab) {
+        safari.application.activeBrowserWindow.activeTab.url = subredditStem + sub;
+    } else {
+        var newTab = safari.application.activeBrowserWindow.openTab();
+        newTab.url = subredditStem + sub;
+    }
+    safari.self.hide();
 }
 
-document.addEventListener('keydown', function (e) {
-  if (e.keyCode === 13) {
-    goToSubreddit(safari.extension.settings.enterNewTab);
-  }
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) {
+        if (e.shiftKey) {
+            goToSubreddit(!safari.extension.settings.enterNewTab);
+        } else {
+            goToSubreddit(safari.extension.settings.enterNewTab);
+        }
+        e.preventDefault();
+    }
 });
